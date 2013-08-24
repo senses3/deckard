ff92c2597499fc3f2a98258908fc31afdf343fb7
nickfletcher <crispus@ohwait.net>
2013-08-24 05:51:47 -0400

    fix indents, move command to rules

--- modules/sandwich.py
+++ modules/sandwich.py
@@ -3,23 +3,24 @@
 sandwich.py - Random Sandwich Module
 
 More info:
-https://github.com/crispus/deckard/
+https://github.com/nickfletcher/deckard/
 """
 
 import random, time
 
-def sandwich(jenni, input):
-		randmsg = random.choice(["Here's a fresh BLT!",
-			"Have a tasty Chicken Carbonara!",
-			"It's dangerous to go alone, take this Pastrami sandwich.",
-			"Everyone loves Chicken Parmesan!",
-			"I hope you like Reuben sandwiches.",
-			"You're a little late, all I have is this PB&J.",
-			"Have a Turkey & Swiss!",
-			"I made this Tuna Melt for you, with love.",
-			"dou told me to give you a Chicken Salad sandwich.",
-			"You won a Manwich.",
-			"Enjoy this Club sandwich on me!"])
-		time.sleep(random.randint(0,1))
-		jenni.reply(randmsg)
-sandwich.commands = ['sandwich']
+def sandwich(phenny, input):
+    randmsg = random.choice(["Here's a fresh BLT!",
+        "Have a tasty Chicken Carbonara!",
+        "It's dangerous to go alone, take this Pastrami sandwich.",
+        "Everyone loves Chicken Parmesan!",
+        "I hope you like Reuben sandwiches.",
+        "You're a little late, all I have is this PB&J.",
+        "Have a Turkey & Swiss!",
+        "I made this Tuna Melt for you, with love.",
+        "dou told me to give you a Chicken Salad sandwich.",
+        "You won a Manwich.",
+        "Enjoy this Club sandwich on me!"])
+    time.sleep(random.randint(0,1))
+    phenny.reply(randmsg)
+sandwich.rule = '(sandwich?.*|sandvich?.*)$'
+sandwich.priority = 'high'
