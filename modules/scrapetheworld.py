6717094accd501a47482287cf6a12b5a76a86475
nickfletcher <crispus@ohwait.net>
2013-08-24 05:45:34 -0400

    update bs3 to bs4, add top reddit post scrape

--- modules/scrapetheworld.py
+++ modules/scrapetheworld.py
@@ -4,8 +4,10 @@ scrapetheworld.py - random scrapes
 
 https://github.com/nickfletcher/deckard/
 """
-from BeautifulSoup import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs
 import urllib2
+
+hdr = { 'User-Agent' : 'rick deckard' }
 
 def commit(phenny, input):
     data = urllib2.urlopen('http://whatthecommit.com').read()
@@ -24,7 +26,7 @@ devex.rule ='(devex?.)$'
 def dinner(phenny, input):
     data = urllib2.urlopen('http://whatthefuckshouldimakefordinner.com').read()
     soup = bs(data)
-    bullshit = soup.dt.dl.string
+    bullshit = soup.dl.string
     meal = soup.a.string
     recipeHref = soup.a['href']
     wtf = bullshit + " " + meal + " " + "(" + recipeHref + ")"
@@ -42,3 +44,15 @@ def zen(phenny, input):
     data = urllib2.urlopen('https://api.github.com/zen').read()
     phenny.say(data)
 zen.rule ='(zen?.)$'
+
+def rtop(phenny, input):
+    url = 'http://reddit.com/r/all'
+    req = urllib2.Request(url, headers=hdr)
+    data = urllib2.urlopen(req).read()
+    soup = bs(data)
+    titleString = soup.select(".entry > .title > .title")[0].string
+    for link in soup.find_all("a", {"class":"title"})[0:1]:
+        href = link['href']
+    title = titleString + " (" + href + ")"
+    phenny.say(title)
+rtop.rule = '(rtop?.)$'
